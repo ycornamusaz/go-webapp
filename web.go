8beba30c01561617a0555a1a612969ab0037b7f9
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2019-05-16 08:00:00 +0200

    Improve IP output

--- web.go
+++ web.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func welcome (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome to my fist go server !")
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to my fist go server !")
 }
 
-func whoami (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Your IP adress is, %q", r.RemoteAddr)
+func whoami(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Your IP adress is, %q", r.Header.Get("X-Forwarded-For"))
 }
 
 func main() {
-    log.Println("Starting web server...")
+	log.Println("Starting web server...")
 
-    http.HandleFunc("/", welcome)
-    http.HandleFunc("/whoami", whoami)
-    err := http.ListenAndServe(":8123", nil)
+	http.HandleFunc("/", welcome)
+	http.HandleFunc("/whoami", whoami)
+	err := http.ListenAndServe(":8123", nil)
 
-    if err != nil {
-        log.Fatalf("Failed to start server:%v", err)
-    }
+	if err != nil {
+		log.Fatalf("Failed to start server:%v", err)
+	}
 }
