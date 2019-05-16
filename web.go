87c48ede0626bc9988051c1c9bcc0b10ca930194
Yoël Cornamusaz <iioel64@gmail.com>
2019-05-16 08:00:00 +0200

    Improve web.go

--- web.go
+++ web.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+    "log"
     "fmt"
     "net/http"
 )
 
 func welcome (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to my fist go server !")
+    fmt.Fprintln(w, "Welcome to my fist go server !")
+}
+
+func whoami (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Your IP adress is, %q", r.RemoteAddr)
 }
 
 func main() {
-    fmt.Println("Strating web server...")
+    log.Println("Starting web server...")
+
     http.HandleFunc("/", welcome)
-    http.ListenAndServe(":8123", nil)
+    http.HandleFunc("/whoami", whoami)
+    err := http.ListenAndServe(":8123", nil)
+
+    if err != nil {
+        log.Fatalf("Failed to start server:%v", err)
+    }
 }
